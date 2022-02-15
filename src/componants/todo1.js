a7d13c0f488672594c578a76d2e472a7d941685d
yousraibrahem1 <yousra.ibrahem@gccpay.sa>
2022-02-15 16:14:33 +0300

    =new to do list

---
+++ src/componants/todo1.js
@@ -0,0 +1,25 @@
+import react from "react";
+import TodoList from "./TodoList";
+
+
+
+
+const todo1 =() =>{
+ return (
+    <div className="todo">
+      <li className="todo-item"> hello todo list </li>
+      <button className="complete-btn">
+        <i className="fas fa-check"></i>
+      </button>
+      <button className="trash-btn">
+        <i className="fas fa-trash"></i>
+      </button>
+    </div>
+  );
+};
+
+
+export default todo1 ;
+
+
+
